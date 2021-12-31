4249708b5941ff7c6ca5782800d581676b5b319c
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-31 18:38:23 +0000

    Miles 2 Kilometer

--- main.py
+++ main.py
@@ -3,29 +3,36 @@ from tkinter import *
 window = Tk()
 window.title("Mile 2 Kilometer Converter")
 window.minsize(width=500, height=300)
-window.config(padx=50, pady=50)
+window.config(padx=100, pady=100)
 
+# Entry
 input = Entry(width=10)
 input.grid(column=1, row=0)
 
+# Label
 mile_label = Label(text="Miles", font=("Arial", 14, "bold"))
 mile_label.grid(column=2, row=0)
 
-equal_label = Label(text="is equal to: ", font=("Arial", 14, "bold"))
+# Label
+equal_label = Label(text="is equal to: ", font=("Arial", 12, "bold"))
 equal_label.grid(column=0, row=1)
 
+# Label
 answer_label = Label(text="0.0", font=("Arial", 14, "bold"))
 answer_label.grid(column=1, row=1)
 
+# Label
 km_label = Label(text="Km", font=("Arial", 14, "bold"))
 km_label.grid(column=2, row=1)
 
 
+# Method
 def calculate_distance():
     answer_km = float(input.get()) * 1.609344
-    answer_label["text"] = round(answer_km, 2)
+    answer_label.config(text=round(answer_km, 2))
 
 
-calculate_button = Button(text="Calculate", command=calculate_distance)
+# Button
+calculate_button = Button(text="Calculate", command=calculate_distance, font=("Arial", 14, "bold"))
 calculate_button.grid(column=1, row=2)
 window.mainloop()
